8e914a74dc8463b0702c457699361d0d49b2af19
dedeepyaadapala <147299058+dedeepyaadapala@users.noreply.github.com>
2025-02-19 20:53:23 +0530

    Time: 1 ms (99.09%), Space: 41 MB (98.41%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -4,11 +4,11 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         int[] x = new int[1];
         x[0] = k;
-        backtrack(n,x,sb,1,' ');
+        backtrack(n,x,sb,' ');
         return sb.toString();
     }
 
-    public boolean backtrack(int n,int[] k,StringBuilder sb,int idx,char prev){
+    public boolean backtrack(int n,int[] k,StringBuilder sb,char prev){
         if(sb.length() == n){
             k[0] -= 1;
             return k[0] == 0;
@@ -16,7 +16,7 @@ class Solution {
         for(char ch = 'a';ch <= 'c';ch++){
             if(ch != prev){
                 sb.append(ch);
-                if(backtrack(n,k,sb,idx+1,ch)) return true;
+                if(backtrack(n,k,sb,ch)) return true;
                 sb.deleteCharAt(sb.length()-1);
             }
         }
