9588712214c6bbf36caa727cb8dcd5aa2ed233a2
dedeepyaadapala <147299058+dedeepyaadapala@users.noreply.github.com>
2025-02-28 18:34:11 +0530

    Time: 57 ms (79.3%), Space: 42.9 MB (61.04%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
@@ -8,22 +8,15 @@ class Solution {
                 int idx = s.charAt(j)-'a';
                 cnt[idx]++;
                 int maxi = -1,mini = n+1;
-                int mxi = -1,mni = -1;
                 for(int k = 0;k < 26;k++){
-                    if(cnt[k] != 0 && maxi < cnt[k]){
+                    if(cnt[k] != 0){
                         maxi = Math.max(maxi,cnt[k]);
-                        mxi = k;
-                    }
-                    if(cnt[k] != 0 && mini > cnt[k]){
                         mini = Math.min(mini,cnt[k]);
-                        mni = k;
                     }
                 }
-                // System.out.println((char)('a'+mxi)+" "+maxi+" "+(char)('a'+mni)+" "+mini);
                 sum += maxi - mini;
 
             }
-            // cnt[s.charAt(i)-'a']--;
         }
         return sum;
     }
