f32d8b83e224b4963cae1e6e839f09193d7cf1ed
dedeepyaadapala <147299058+dedeepyaadapala@users.noreply.github.com>
2025-03-25 19:33:14 +0530

    Time: 0 ms (100%), Space: 8.3 MB (52.5%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.c
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -1,11 +1,11 @@
 int searchInsert(int* nums, int n, int target) {
-    int i;
-    for(i = 0;i < n;i++){
-        if(nums[i] >= target){
-            return i;
-        }
-    }
-    return n;
+    // int i;
+    // for(i = 0;i < n;i++){
+    //     if(nums[i] >= target){
+    //         return i;
+    //     }
+    // }
+    // return n;
 
 
     int low = 0,high = n-1;
