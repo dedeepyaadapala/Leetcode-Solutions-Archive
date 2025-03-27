0ca84d62a5582a3617ffd3c318f7262e649431d9
dedeepyaadapala <147299058+dedeepyaadapala@users.noreply.github.com>
2025-03-27 12:30:27 +0530

    Time: 11 ms (95.51%), Space: 56.9 MB (75.64%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -1,24 +1,29 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
         int n = nums.size();
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i : nums){
-            map.put(i,map.getOrDefault(i,0)+1);
-        }
-        HashMap<Integer,Integer> mp = new HashMap<>();
-        int cnt = 0,ele = nums.get(0);
-        for(int i = 0;i < n;i++){
-            int val = nums.get(i);
-            mp.put(val,mp.getOrDefault(val,0)+1);
-            if(val == ele){
+        int majority = nums.get(0),cnt = 0;
+        for(int i: nums){
+            if(i == majority){
                 cnt++;
-            }else if(cnt < 0){
-                ele = val;
+            }else if(cnt == 0){
+                majority = i;
                 cnt = 1;
             }else{
                 cnt--;
             }
-            if(mp.get(val) > (i+1) / 2 && map.get(val) - mp.get(val) > (n-i-1)/2){
+        }
+        cnt = 0;
+        for(int i: nums){
+            if(i == majority){
+                cnt++;
+            }
+        }
+        int curr_cnt = 0;
+        for(int i = 0;i < n;i++){
+            if(nums.get(i) == majority){
+                curr_cnt++;
+            }
+            if(curr_cnt > (i+1)/2 && cnt - curr_cnt > (n-i-1)/2){
                 return i;
             }
         }
