247573a4b1ccae13aa865d9cc8eaacaf892cbce4
dedeepyaadapala <147299058+dedeepyaadapala@users.noreply.github.com>
2025-04-01 17:18:09 +0530

    Time: 8 ms (47.5%), Space: 108.3 MB (59.11%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
@@ -2,7 +2,20 @@ class Solution {
     public long mostPoints(int[][] questions) {
         int n = questions.length;
         long dp[] = new long[n];
-        return findRes(questions,n,0,dp);
+        dp[n-1] = questions[n-1][0];
+        for(int i = n-2;i >= 0;i--){
+            int next = i + questions[i][1] + 1;
+            if(next < n){
+                dp[i] = Math.max(dp[next]+questions[i][0],dp[i+1]);
+            }
+            dp[i] = Math.max(dp[i+1],Math.max(questions[i][0],dp[i]));
+        }
+        long res = 0;
+        for(int i = 0;i < n;i++){
+            res = Math.max(res,dp[i]);
+        }
+        return res;
+        // return findRes(questions,n,0,dp);
     }
 
     private static long findRes(int[][] questions,int n,int i,long[] dp){
