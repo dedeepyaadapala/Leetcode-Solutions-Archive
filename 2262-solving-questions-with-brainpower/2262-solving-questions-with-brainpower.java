34765ec6b72e95677bb08ce80bd0331ffbb93e23
dedeepyaadapala <147299058+dedeepyaadapala@users.noreply.github.com>
2025-04-01 17:19:36 +0530

    Time: 8 ms (47.5%), Space: 108 MB (79.64%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
@@ -18,13 +18,6 @@ class Solution {
         // return findRes(questions,n,0,dp);
     }
 
-    private static long findRes(int[][] questions,int n,int i,long[] dp){
-        if(i >= n) return 0;
-        if(dp[i] != 0) return dp[i];
-        long pick = questions[i][0] + findRes(questions,n,i+questions[i][1]+1,dp);
-        long notPick = findRes(questions,n,i+1,dp);
-        return dp[i] = Math.max(pick,notPick);
-    }
 }
 
 
