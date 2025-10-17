25cd81b1a7e5bbb4e2f2a7a22092442c6f08e08c
dedeepyaadapala <147299058+dedeepyaadapala@users.noreply.github.com>
2025-10-17 08:52:29 +0530

    Time: 2 ms (86.2%), Space: 44.8 MB (76.75%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,69 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for(int i = 0;i < n;i++){
+            Arrays.fill(board[i],'.');
+        }
+        nQueens(n,0,res,board);
+        return res;
+    }
+
+    private static void nQueens(int n,int idx,List<List<String>> res,char[][] board){
+        if(idx == n){
+            // if(isValid(board,n)){
+                List<String> curr = new ArrayList<>();
+                for(char[] str : board){
+                    curr.add(new String(str));
+                }
+                res.add(curr);
+            // }
+            return;
+        }
+        for(int j = 0;j < n;j++){
+            if(canPlace(board,idx,j,n)){
+                board[idx][j] = 'Q';
+                nQueens(n,idx+1,res,board);
+                board[idx][j] = '.';
+            }
+        }
+    }
+
+    private static boolean canPlace(char[][] board,int row,int col,int n){
+        for(int i = row-1;i >= 0;i--){
+            if(board[i][col] == 'Q') return false;
+        }
+        for(int i = row-1,j = col-1;i >= 0 && j >= 0;i--,j--){
+            if(board[i][j] == 'Q') return false;
+        }
+        for(int i = row-1,j = col+1;i >= 0 && j < n;i--,j++){
+            if(board[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+
+    private static boolean isValid(char[][] board,int n){
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < n;j++){
+                if(board[i][j] == 'Q'){
+                    for(int k = 0;k < n;k++){
+                        if(k != j && board[i][k] == 'Q'){
+                            return false;
+                        }
+                    }
+                    for(int k = i+1,l = j-1;k < n && l >= 0;k++,l--){
+                        if(board[k][l] == 'Q'){
+                            return false;
+                        }
+                    }
+                    for(int k = i+1,l = j+1;k < n && l < n;k++,l++){
+                        if(board[k][l] == 'Q'){
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
