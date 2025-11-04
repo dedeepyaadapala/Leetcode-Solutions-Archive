50e8441bc8e46a3f6850e2fa8ae8e55195d5ac2e
dedeepyaadapala <147299058+dedeepyaadapala@users.noreply.github.com>
2025-11-04 18:50:08 +0530

    Time: 1 ms (90.19%), Space: 43.4 MB (10.16%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,18 +1,23 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         if(s3.length() != s1.length() +s2.length()) return false;
-        if(s1.equals("abababababababababababababababababababababababababababababababababababababababababababababababababbb") && s2.equals("babababababababababababababababababababababababababababababababababababababababababababababababaaaba") && s3.equals("abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababbb")) return false;
-        return interleaving(s1,0,s1.length(),s2,0,s2.length(),s3,0,s3.length());
+        int[][] dp = new int[s1.length()+1][s2.length()+1];
+        for(int i = s1.length() - 1;i >= 0;i--){
+            Arrays.fill(dp[i],-1);
+        }
+        return interleaving(s1,0,s1.length(),s2,0,s2.length(),s3,0,s3.length(),dp) == 1;
     }
 
-    private static boolean interleaving(String s1,int idx1,int n1,String s2,int idx2,int n2,String s3,int idx3,int n3){
-        if(idx3 == n3) return true;
+    private static int interleaving(String s1,int idx1,int n1,String s2,int idx2,int n2,String s3,int idx3,int n3,int[][] dp){
+        if(idx3 == n3) return 1;
+        if(idx1 < n1 && idx2 < n2 && dp[idx1][idx2] != -1) return dp[idx1][idx2];
+        int ans = 0;
         if(idx1 < n1 && s1.charAt(idx1) == s3.charAt(idx3)){
-            if(interleaving(s1,idx1+1,n1,s2,idx2,n2,s3,idx3+1,n3)) return true;
+            ans = ans | (interleaving(s1,idx1+1,n1,s2,idx2,n2,s3,idx3+1,n3,dp));
         }
         if(idx2 < n2 && s2.charAt(idx2) == s3.charAt(idx3)){
-            if(interleaving(s1,idx1,n1,s2,idx2+1,n2,s3,idx3+1,n3)) return true;
+            ans = ans | (interleaving(s1,idx1,n1,s2,idx2+1,n2,s3,idx3+1,n3,dp));
         }
-        return false;
+        return dp[idx1][idx2] = ans;
     }
 }
