4157567d17f6b07a6efe45442d818fd086d5d800
dedeepyaadapala <147299058+dedeepyaadapala@users.noreply.github.com>
2025-11-25 20:09:23 +0530

    Time: 3 ms (17%), Space: 42 MB (59.41%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.java
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.java
@@ -3,8 +3,9 @@ class Solution {
         HashSet<Integer> set = new HashSet<>();
         int n = 1;
         for(int i = 1;i <= k;i++){
-            if(n % k == 0) return i;
-            n = ((n % k) * 10) + 1;
+            int rem = n % k;
+            if(rem == 0) return i;
+            n = (rem * 10) + 1;
         }
         return -1;
     }
