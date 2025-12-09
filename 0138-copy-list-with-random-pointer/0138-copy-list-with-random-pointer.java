744a08a39f5d76b5c21a92a8e4996ab74ad36e46
dedeepyaadapala <147299058+dedeepyaadapala@users.noreply.github.com>
2025-12-09 19:42:08 +0530

    Time: 0 ms (100%), Space: 46.8 MB (12.41%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -1,41 +1,65 @@
-/*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
-*/
-
 class Solution {
     public Node copyRandomList(Node head) {
-        if(head == null) return null;
-        HashMap<Node,Node> map = new HashMap<>();
-        Node th = head;
-        Node temp = null;
-        while(head != null){
-            if(!map.containsKey(head)){
-                map.put(head,new Node(head.val));
-            }
-            temp = map.get(head);
-            if(head.next != null){
-                map.putIfAbsent(head.next,new Node(head.next.val));
-                temp.next = map.get(head.next);
-            }
-
-            if(head.random != null){
-                map.putIfAbsent(head.random,new Node(head.random.val));
-                temp.random = map.get(head.random);
-            }
-            head = head.next;
-            temp = temp.next;
+        if (head == null)
+            return head;
+        Node t1 = head;
+        Node h = clone(head);
+        Node t2 = h;
+        while (t1 != null && t2 != null) {
+            // Node nn=t1.next;
+            // t1.next=t2;
+            // Node nn2=t2.next;
+            // t2.next=nn;
+            // t1=nn;
+            // t2=nn2;
+            Node temp = t2.next;
+            t2.next = t1.next;
+            t1.next = t2;
+            t1 = t1.next.next;
+            t2 = temp;
         }
-        return map.get(th);
+        // display(head);
+
+        Node h1 = head;
+        Node h2 = h;
+        while (h2 != null && h1 != null) {
+            if (h1.random != null) {
+                h2.random = h1.random.next;
+            }
+            h1 = h1.next.next;
+            if (h2.next != null)
+                h2 = h2.next.next;
+        }
+        Node h3 = head;
+        Node h4 = head.next;
+        Node h5 = h4;
+        while (h3 != null && h4 != null && h4.next != null) {
+            h3.next = h4.next;
+            h4.next = h3.next.next;
+            h3 = h3.next;
+            h4 = h4.next;
+        }
+        h3.next = null;
+        if(h4 != null) h4.next = null;
+        //display(h5);
+        return h5;
+    }
+
+    void display(Node h) {
+        while (h != null) {
+            System.out.print(h.val);
+            h = h.next;
+        }
+    }
+
+    private Node clone(Node h) {
+        Node dummy = new Node(-1);
+        Node temp = dummy;
+        while (h != null) {
+            dummy.next = new Node(h.val);
+            dummy = dummy.next;
+            h = h.next;
+        }
+        return temp.next;
     }
 }
